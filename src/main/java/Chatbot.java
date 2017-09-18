259973b1b2aab31bbc98ecbd5d11c040f8685d94
Evgenii_Lartcev <Evgenii_Lartcev@epam.com>
2017-09-18 19:07:42 +0300

    improved

--- src/main/java/Chatbot.java
+++ src/main/java/Chatbot.java
@@ -10,55 +10,60 @@ import org.alicebot.ab.MagicStrings;
 import org.alicebot.ab.utils.IOUtils;
 
 import java.io.File;
+import java.util.logging.Logger;
+
 
 public class Chatbot {
-    private static final boolean TRACE_MODE = false;
-    static String botName = "super";
+
+    private static final Logger LOGGER = Logger.getLogger(Chatbot.class.getName());
+
+    private static final boolean TRACE_MODE = true;
+
+    private static final String BOT_NAME = "super";
 
     public static void main(String[] args) {
-        try {
+        final String resourcesPath = getResourcesPath();
+        LOGGER.info(resourcesPath);
 
-            String resourcesPath = getResourcesPath();
-            System.out.println(resourcesPath);
-            MagicBooleans.trace_mode = TRACE_MODE;
-            Bot bot = new Bot("super", resourcesPath);
-            Chat chatSession = new Chat(bot);
-            bot.brain.nodeStats();
-            String textLine = "";
+        MagicBooleans.trace_mode = TRACE_MODE;
+        final Bot bot = new Bot(BOT_NAME, resourcesPath);
+        bot.brain.nodeStats();
+        final Chat chatSession = new Chat(bot);
 
-            while (true) {
-                System.out.print("Human : ");
-                textLine = IOUtils.readInputTextLine();
-                if ((textLine == null) || (textLine.length() < 1))
-                    textLine = MagicStrings.null_input;
-                if (textLine.equals("q")) {
-                    System.exit(0);
-                } else if (textLine.equals("wq")) {
-                    bot.writeQuit();
-                    System.exit(0);
-                } else {
-                    String request = textLine;
-                    if (MagicBooleans.trace_mode)
-                        System.out.println("STATE=" + request + ":THAT=" + ((History) chatSession.thatHistory.get(0)).get(0) + ":TOPIC=" + chatSession.predicates.get("topic"));
-                    String response = chatSession.multisentenceRespond(request);
-                    while (response.contains("&lt;"))
-                        response = response.replace("&lt;", "<");
-                    while (response.contains("&gt;"))
-                        response = response.replace("&gt;", ">");
-                    System.out.println("Robot : " + response);
-                }
+        String textLine = "";
+
+        while (true) {
+            System.out.print("Human : ");
+            textLine = IOUtils.readInputTextLine();
+            if ((textLine == null) || (textLine.length() < 1))
+                textLine = MagicStrings.null_input;
+            if (textLine.equals("q")) {
+                LOGGER.warning("Receive command to exit from a chat");
+                System.exit(0);
+            } else if (textLine.equals("wq")) {
+                LOGGER.warning("This will rewrite some data in some files");
+                bot.writeQuit();
+                System.exit(0);
+            } else {
+                String request = textLine;
+                if (MagicBooleans.trace_mode)
+                    LOGGER.warning("STATE=" + request + ":THAT=" + ((History) chatSession.thatHistory.get(0)).get(0) + ":TOPIC=" + chatSession.predicates.get("topic"));
+                String response = chatSession.multisentenceRespond(request);
+                while (response.contains("&lt;"))
+                    response = response.replace("&lt;", "<");
+                while (response.contains("&gt;"))
+                    response = response.replace("&gt;", ">");
+                System.out.println("Robot : " + response);
             }
-        } catch (Exception e) {
-            e.printStackTrace();
         }
+
     }
 
     private static String getResourcesPath() {
-        File currDir = new File(".");
+        final File currDir = new File(".");
         String path = currDir.getAbsolutePath();
-        path = path.substring(0, path.length() - 2);
-        System.out.println(path);
-        String resourcesPath = path + File.separator + "src" + File.separator + "main" + File.separator + "resources";
-        return resourcesPath;
+        path = path.substring(0, path.length() - 2); //delete '\.' symbols at the end
+        LOGGER.info(path);
+        return path + File.separator + "src" + File.separator + "main" + File.separator + "resources";
     }
 }
